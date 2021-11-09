65c17c6b6109f63f309815d77b3f061e7003e29c
RamonCosta07 <ramonnar20@hotmail.com>
2021-11-09 16:54:16 -0300

    Dois desafios usando .net

---
+++ Desafio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Leia quatro valores inteiros A, B, C e D
+             * A seguir, calcule e mostre a diferença
+             * do produto de A e B pelo produto
+             * de C e D segundo a fórmula:
+             * DIFERENCA = (A * B - C * D).
+             */
+
+            int a, b, c, d;   //declare suas variaveis             
+
+            Console.WriteLine("Digite o valor de a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de c: ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de d: ");
+            d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"DIFERENCA = {a * b - c * d}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Desafio2/Desafio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Calcule e imprima a variável MEDIA
+             * com 5 dígitos após o ponto decimal
+             * e com um espaço em branco antes e
+             * depois da igualdade.
+             */
+            double a, b, media;
+
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            a = Math.Round(a, 2);
+            b = Math.Round(b, 2);
+
+            media = Math.Round(((a * 3.5) + (b * 7.5)) / (3.5 + 7.5), 5);
+
+            Console.WriteLine($"MEDIA = {media}");
+        }
+    }
+}
